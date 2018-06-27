ecd716cb33e427edd95def3e0477d8fc688d401d
warlue <wanglu2325@qq.com>
2018-06-28 00:29:27 +0800

    添加数据源，mybatis配置文件

--- src/main/java/com/thsware/controller/QdjjController.java
+++ src/main/java/com/thsware/controller/QdjjController.java
@@ -1,7 +1,14 @@
 package com.thsware.controller;
 
-//清单计价
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class QdjjController {
     //
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 
 }
